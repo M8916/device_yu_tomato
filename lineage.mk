7584fe2bb6d60e3bdf1cc174eaee4eb9b3539e21
firebird11 <hbgassel@gmail.com>
2018-04-25 13:52:05 +0000

    set build fingerprint instead of override

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,4 @@
-# Copyright (C) 2014 The CyanogenMod Project
-#
+# Copyright (C) 2014 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -28,11 +27,14 @@ PRODUCT_GMS_CLIENTID_BASE := android-micromax
 
 TARGET_VENDOR_PRODUCT_NAME := YUREKA
 TARGET_VENDOR_DEVICE_NAME := YUREKA
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=YUREKA PRODUCT_NAME=YUREKA
+PRODUCT_BUILD_PROP_OVERRIDES +=
+      TARGET_DEVICE=YUREKA \
+      PRODUCT_NAME=YUREKA
 
 ## Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=YU/YUREKA/YUREKA:5.1.1/LMY49J/YOG4PAS8A4:user/release-keys \
     PRIVATE_BUILD_DESC="YUREKA-user 5.1.1 LMY49J YOG4PAS8A4 release-keys"
 endif
+
+BUILD_FINGERPRINT := "YU/YUREKA/YUREKA:5.1.1/LMY49J/YOG4PAS8A4:user/release-keys"
